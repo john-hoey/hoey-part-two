7dbb3dbabe75e0e68ffd38672c7104f2351ba568
john <john.t.hoey@gmail.com>
2020-12-11 20:04:09 -0500

    Second draft of javascript

--- js/script.js
+++ js/script.js
@@ -1,71 +1,113 @@
 let rainInches = null;
-let bushelYield = null;
+let rainDisplay = null;
 let estimatedBushelYield = 50;
+let bushelYield = null;
 let usedFertilizer = null;
 let qualityFertilizer = null;
+let finalYield = null;
+
 function isNumber(value) {
   const numberPattern = /^[0-9]+$/;
   return numberPattern.test(value);
 }
-rainInches = prompt("How much rain fell in inches? (Please use digits)");
-while (rainInches === null || !isNumber(rainInches)) {
-  rainInches = prompt(
-    "That is not a valid number. Please enter the amount of rain fall in inches."
-  );
-}
-s = "*".repeat(rainInches);
-console.log(s);
 
-if (rainInches >= 20) {
-  bushelYield = estimatedBushelYield * 0.9;
-  console.log(bushelYield);
-} else if (rainInches <= 10) {
-  bushelYield = estimatedBushelYield * 0.8;
-  console.log(bushelYield);
-} else {
-  bushelYield = 50;
-  console.log(bushelYield);
+function rainfallAsk() {
+  rainInches = prompt("How many inches of rain fell?");
+  while (rainInches === null || !isNumber(rainInches)) {
+    rainInches = prompt(
+      "That is not a valid number. Please enter the amount of rain in inches using digits. (0-9)"
+    );
+  }
 }
-while (bushelYield != null) {
-  usedFertilizer = prompt("Did you use fertilizer? (Yes/No)");
+
+rainfallAsk();
+rainDisplay = "*".repeat(rainInches);
+console.log(rainDisplay);
+
+function rainYieldCalc(a) {
+  if (rainInches >= 20) {
+    bushelYield = estimatedBushelYield * 0.9;
+    console.log(
+      "Accounting for a wetter than average season, your estimated yield is " +
+        bushelYield +
+        " bushels per acre."
+    );
+  } else if (rainInches <= 10) {
+    bushelYield = estimatedBushelYield * 0.8;
+    console.log(
+      "Accounting for a dryer than average season, your estimated yield is " +
+        bushelYield +
+        " bushels per acre."
+    );
+  } else {
+    bushelYield = estimatedBushelYield;
+    console.log(
+      "Accounting for an average amount of rainfall this season, your estimated yield is " +
+        bushelYield +
+        " bushels per acre."
+    );
+  }
 }
-while (
-  usedFertilizer === "yes" ||
-  usedFertilizer === "Yes" ||
-  usedFertilizer === "YES"
-) {
-  qualityFertilizer = prompt("did you use regular or premium fertilizer?");
+
+rainYieldCalc(rainInches);
+
+function fertAsk() {
+  usedFertilizer = prompt("Did you use fertilizer?");
+  while (
+    usedFertilizer === null ||
+    !(usedFertilizer == "yes" || usedFertilizer == "no")
+  ) {
+    usedFertilizer = prompt(
+      "That is not a valid answer. Please answer yes or no. Did you use fertilizer?"
+    );
+  }
+  if (usedFertilizer == "no") {
+    console.log("Without fertilizer you didn't get a boosted yield");
+    console.log("Average Season's Bushel Yield: " + estimatedBushelYield);
+    console.log(
+      "Your Estimated Bushel Yield Accounting for Rainfall: " + bushelYield
+    );
+    console.log("Actual Bushel Yield: " + bushelYield);
+  }
 }
-while (
-  usedFertilizer === "no" ||
-  usedFertilizer === "No" ||
-  usedFertilzier === "NO"
-) {
-  console.log("Without fertilizer you didn't get a boosted yield");
-  console.log("Estimated Bushel Yield:" + estimatedBushelYield);
-  console.log("Actual Bushel Yield:" + bushelYield);
+
+fertAsk();
+
+function fertType() {
+  qualityFertilizer = prompt("Did you use regular or premium fertilizer?");
+  while (
+    qualityFertilizer === null ||
+    !(qualityFertilizer == "regular" || qualityFertilizer == "premium")
+  ) {
+    qualityFertilizer = prompt(
+      "That is not a valid answer. Please answer regular or premium."
+    );
+  }
+  if (qualityFertilizer == "regular") {
+    finalYield = bushelYield * 1.1;
+    console.log(
+      "With regular fertilizer you had a 10% boost to your crop yield!"
+    );
+    console.log("Average Season's Bushel Yield: " + estimatedBushelYield);
+    console.log(
+      "Your Estimated Bushel Yield Accounting for Rainfall: " + bushelYield
+    );
+    console.log("Actual Bushel Yield: " + finalYield);
+  } else if (qualityFertilizer == "premium") {
+    finalYield = bushelYield * 1.15;
+    console.log(
+      "With premium fertilizer you had a 15% boost to your crop yield!"
+    );
+    console.log("Average Season's Bushel Yield: " + estimatedBushelYield);
+    console.log(
+      "Your Estimated Bushel Yield Accounting for Rainfall: " + bushelYield
+    );
+    console.log("Actual Bushel Yield: " + finalYield);
+  } else {
+    qualityFertilizer = prompt("Did you use regular or premium fertilizer?");
+  }
 }
-while (
-  qualityFertilizer === "regular" ||
-  qualityFertilizer === "Regular" ||
-  qualityFertilizer === "REGULAR"
-) {
-  bushelYield = bushelYield * 1.1;
-  console.log(
-    "With regular fertilizer you had a 10% boost to your crop yield!"
-  );
-  console.log("Estimated Bushel Yield:" + estimatedBushelYield);
-  console.log("Actual Bushel Yield:" + bushelYield);
-}
-while (
-  qualityFertilizer === "premium" ||
-  qualityFertilizer === "Premium" ||
-  qualityFertilizer === "PREMIUM"
-) {
-  bushelYield = bushelYield * 1.15;
-  console.log(
-    "With premium fertilizer you had a 15% boost to your crop yield!"
-  );
-  console.log("Estimated Bushel Yield:" + estimatedBushelYield);
-  console.log("Actual Bushel Yield:" + bushelYield);
+
+if (usedFertilizer == "yes") {
+  fertType();
 }
